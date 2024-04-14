cd7756396b185896d581d2e82d10c122f4636ebd
Alberto Lopez <alberto.lopez_g@outlook.com>
2024-04-14 09:36:48 -0700

    add controller, service and module control

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+  @Post()
+  public method(@Body() body: any) {
+    return body;
+  }
+}
